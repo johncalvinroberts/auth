722ae479ae9027996c05809cced4f9a06a400352
Harminder Virk <virk.officials@gmail.com>
2023-10-30 16:53:15 +0530

    refactor: remove loginRoute from auth config

--- src/auth/define_config.ts
+++ src/auth/define_config.ts
@@ -27,7 +27,6 @@ export type ResolvedAuthConfig<
   KnownGuards extends Record<string, GuardFactory | GuardConfigProvider<GuardFactory>>,
 > = {
   default: keyof KnownGuards
-  loginRoute: string
   guards: {
     [K in keyof KnownGuards]: KnownGuards[K] extends GuardConfigProvider<infer A>
       ? A
@@ -44,7 +43,6 @@ export function defineConfig<
   KnownGuards extends Record<string, GuardFactory | GuardConfigProvider<GuardFactory>>,
 >(config: {
   default: keyof KnownGuards
-  loginRoute: string
   guards: KnownGuards
 }): ConfigProvider<ResolvedAuthConfig<KnownGuards>> {
   return configProvider.create(async (app) => {
@@ -62,7 +60,6 @@ export function defineConfig<
 
     return {
       default: config.default,
-      loginRoute: config.loginRoute,
       guards: guards,
     } as ResolvedAuthConfig<KnownGuards>
   })
