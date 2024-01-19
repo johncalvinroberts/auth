19bb212d7943bd6663a64c9b546b60895108b19d
Harminder Virk <virk.officials@gmail.com>
2024-01-19 12:02:33 +0530

    refactor: update configure command

--- configure.ts
+++ configure.ts
@@ -15,10 +15,47 @@ import type Configure from '@adonisjs/core/commands/configure'
  */
 export async function configure(command: Configure) {
   const codemods = await command.createCodemods()
-  // let guard: string | undefined = command.parsedFlags.guard
+  let guard: string | undefined = command.parsedFlags.guard
+
+  /**
+   * Prompts user to select a guard when not mentioned via
+   * the CLI
+   */
+  if (guard === undefined) {
+    guard = await command.prompt.choice(
+      'Select the auth guard you want to use',
+      [
+        {
+          name: 'session',
+          message: 'Session',
+        },
+        {
+          name: 'access_tokens',
+          message: 'Opaque access tokens',
+        },
+      ],
+      {
+        validate(value) {
+          return !!value
+        },
+      }
+    )
+  }
+
+  /**
+   * Ensure selected or guard defined via the CLI flag is
+   * valid
+   */
+  if (!['session', 'access_tokens'].includes(guard!)) {
+    command.logger.error(
+      `The selected guard "${guard}" is invalid. Select one from: session, access_tokens`
+    )
+    command.exitCode = 1
+    return
+  }
 
   await presetAuth(codemods, command.app, {
-    guard: 'session',
+    guard: guard as 'session' | 'access_tokens',
     userProvider: 'lucid',
   })
 }
